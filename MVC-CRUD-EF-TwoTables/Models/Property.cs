e4add9271790d5ccf70d3a9660de4fa93e69e98e
gyan <gyan@BE-1034-INS>
2026-02-23 13:36:39 -0600

    Add project files.

---
+++ MVC-CRUD-EF-TwoTables/Models/Property.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_CRUD_EF_TwoTables.Models
+{
+    public class Property
+    {
+        [Key]
+        public int PropertyId { get; set; }
+        [Required(ErrorMessage="Please enter the property name")]
+        [StringLength(100)]
+        public string PropertyName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Address { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string UnitNumber { get; set; }
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Please enter a valid rent amount")]
+        public decimal MonthlyRent { get; set; }
+
+        // Navigation property for related tenants
+        public virtual ICollection<Tenant> Tenants { get; set; }= new List<Tenant>();
+    }
+}
---
+++ MVC-CRUD-EF-TwoTables/Models/Tenant.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_CRUD_EF_TwoTables.Models
+{
+    public class Tenant
+    {
+        [Key]
+        public int TenantID { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+        [Required]
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
+        public string PhoneNumber { get; set; }
+
+        // Navigation property for related property
+        public virtual Property? Property { get; set; }
+    }
+}
